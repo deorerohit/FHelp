a44a8425502b5156b2e970af9756d81a3dcc890c
deorerohit <deorerohit18@gmail.com>
2021-09-15 18:59:42 +0530

    creater sign up interface

--- app/src/main/java/be/project/farmhelp/SignIn.java
+++ app/src/main/java/be/project/farmhelp/SignIn.java
@@ -1,8 +1,10 @@
 package be.project.farmhelp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignIn extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void callSingUp(View view) {
+        startActivity(new Intent(SignIn.this, SignUp.class));
     }
 }
